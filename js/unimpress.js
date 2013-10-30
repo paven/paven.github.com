f6ac5ce06fe04c539fa2ca3e4db98e6444c9da50
Patrik Gustafsson <paven@mumma.nu>
2013-10-30 16:09:12 +0100

    Revert to 46fe2becb57233ef8b045a99d3bcd1e4f73b8db5

--- js/unimpress.js
+++
@@ -1,9 +0,0 @@
-
-function goToByScroll(id){
-    $("#"+id).parents().animate({
-      scrollTop: $("#"+id).offset().top,
-      scrollLeft: $("#"+id).offset().left  
-    }, 'slow', function() {
-    // Animation complete.
-  });       
-}
